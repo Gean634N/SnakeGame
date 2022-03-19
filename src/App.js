2f8769761cca97756febfb5b7d51f52121ab027f
Gean634N <gean.a.l.c@gmail.com>
2022-03-19 11:27:03 -0400

    Setup inicial e tela de jogo
    Deleta arquivos que nao serao necessarios e cria a primeira div que sera usada como tela/area de jogo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="game-screen">
+        <p>
+          Hello World!
+        </p>
+    </div>
+  );
+}
+
+export default App;
